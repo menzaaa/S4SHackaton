d0dcdb8796721a2bde85c92ece80dae1ec97bfe9
Menno Prinzhorn <menno@create.nl>
2018-09-20 14:17:11 +0200

    questions

--- front-end/src/components/quizzes/Create.vue
+++ front-end/src/components/quizzes/Create.vue
@@ -1,24 +1,60 @@
 <template>
     <div class="create-quiz">
-        <form>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class="row justify-content-center">
+            <div class="col col-lg-8">
+                <div class="card">
+                    <div class="card-header">
+                        Create your quiz
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Name</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+                            </div>
+                            <button type="submit" class="btn btn-primary create-quiz-btn">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data () {
+            return {
+                loading: false
+            }
+        },
+        created () {
+            this.fetchData()
+        },
+        mounted () {
+            this.setEventListeners()
+        },
+        methods: {
+            fetchData () {
+                this.loading = true
+                setTimeout(this.setTimeout, 1000)
+            },
+            setTimeout() {
+                this.loading = false
+            },
+            setEventListeners() {
+                const submitBtn = document.querySelector('.create-quiz-btn')
+                if(submitBtn){
+                    submitBtn.addEventListener('click', () => {
+                        this.createQuiz()
+                    })
+                }
+            },
+            createQuiz() {
+                //doe api call
+                alert('Quiz aangemaakt');
+                this.$router.push({ name: 'quizzes'})
+            }
+        },
+    }
 </script>
