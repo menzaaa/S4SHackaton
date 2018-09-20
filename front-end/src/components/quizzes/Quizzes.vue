bf2fc5e63083269256f5b7be60ba7a9471af02c7
Max van Hasselt <maxvanhasselt@hotmail.nl>
2018-09-20 17:54:33 +0200

    Delete quizzes works

--- front-end/src/components/quizzes/Quizzes.vue
+++ front-end/src/components/quizzes/Quizzes.vue
@@ -9,18 +9,20 @@
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Name</th>
-                    <th scope="col">Creator</th>
+                    <!-- <th scope="col">Creator</th> -->
                     <th scope="col">Attend</th>
                     <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="quiz in quizzes" :key="quiz.id">
+                <tr v-for="quiz in quizzes" :key="quiz.id" v-bind:data-quiz-id="quiz.id">
                     <th scope="row">{{ quiz.id }}</th>
                     <td>{{ quiz.name }}</td>
-                    <td>{{ quiz.user.first_name }} {{ quiz.user.last_name }}</td>
+                    <!-- <td>{{ quiz.user.first_name }} {{ quiz.user.last_name }}</td> -->
                     <td><router-link :to="{ name: 'quiz', params: { id: quiz.id}}">Here</router-link></td>
                     <td><router-link :to="{ name: 'quiz.edit', params: { id: quiz.id}}">Here</router-link></td>
+                    <td><button v-on:click='deleteQuiz(quiz.id)'>Delete</button></td>
                 </tr>
             </tbody>
         </table>
@@ -28,7 +30,13 @@
     </div>
 </template>
 
-<script>
+<script> 
+
+    const axios = require('axios')
+	const instance = axios.create({
+		baseURL: 'http://localhost:5000/'
+    })
+    
     export default {
         data () {
             return {
@@ -36,13 +44,26 @@
                 loading: false
             }
         },
+        mounted(){
+            this.setEventListeners()
+        },
         created () {
             this.fetchData()
         },
         methods: {
+
+
             fetchData () {
+                const axios = require('axios')
+				instance.defaults.headers.common['Authorization'] = 'Basic ' + btoa('max' + ':' + 'qwerty');
+				const that = this
+				instance.get('/quizzes')
+					.then(function (response) {
+                        that.quizzes = response.data
+					});
+
                 this.loading = true
-                setTimeout(this.setTimeout, 1000)
+                // setTimeout(this.setTimeout, 1000)
             },
             setTimeout() {
                 this.loading = false
@@ -52,7 +73,32 @@
                     { id: 3, name: "quiz 3", user: { first_name: "menno", last_name: "prinzhorn"}},
                     { id: 4, name: "quiz 4", user: { first_name: "menno", last_name: "prinzhorn"}},
                 ]
+            },
+            setEventListeners() {
+                const submitBtn = document.querySelector('.delete-quiz-btn')
+                console.log(submitBtn);
+                if(submitBtn){
+                    submitBtn.addEventListener('click', () => {
+                        this.deleteQuiz()
+                    })
+                }
+            },
+            deleteQuiz(id){
+                const axios = require('axios')
+				instance.defaults.headers.common['Authorization'] = 'Basic ' + btoa('max' + ':' + 'qwerty');
+                const that = this
+                var a = document.querySelector('tr[data-quiz-id="' + id +'"]');
+                console.log(a);
+				instance.delete('/quizzes/' + id )
+					.then(function (response) {
+                        console.log(response);
+                        // var a = document.querySelector('a[data-quiz-id="1"]');
+                        // console.log(a);
+                        a.remove().remove();
+					});
+                // axios.delete('/quiz/' + id)
             }
         },
+        
     }
 </script>
